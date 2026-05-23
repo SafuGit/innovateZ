11566a67073f792342ed7c7b45ca6689c3013a7a
Safwan Sadid <safwansadid.ah@gmail.com>
2026-05-23 19:31:30 +0600

    Add redesigned contact page layout and contact information data

---
+++ src/data/contact.ts
@@ -0,0 +1,15 @@
+const contactInfo = {
+  email: 'innovatez.bd@gmail.com',
+  phone: '+880 1560027206',
+  tel: '+8801560027206',
+  whatsapp: '+8801560027206',
+  whatsappUrl: 'https://wa.me/14155550200',
+  linkedinHandle: '/company/innovatez',
+  linkedinUrl: 'https://www.linkedin.com/company/innovatez',
+  instagramHandle: '@innovatez.bd',
+  instagramUrl: 'https://www.instagram.com/innovatez.bd/',
+  facebookHandle: '/profile.php?id=61590147622040',
+  facebookUrl: 'https://www.facebook.com/profile.php?id=61590147622040'
+};
+
+export default contactInfo;
