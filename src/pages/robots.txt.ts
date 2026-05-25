f5231c679368658ee6ee51b6f3ac7c3b709f0fa1
Safwan Sadid <safwansadid.ah@gmail.com>
2026-05-25 13:53:52 +0600

    fix: update sitemap URLs to use the correct domain

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -8,10 +8,10 @@ Disallow: /private
 Crawl-delay: 1
 
 # Specify sitemap
-Sitemap: https://innovatez.com/sitemap-index.xml
+Sitemap: https://innovatez.tech/sitemap-index.xml
 
 # Additional search engines
-Sitemap: https://innovatez.com/sitemap-0.xml
+Sitemap: https://innovatez.tech/sitemap-0.xml
 `, {
     headers: {
       'Content-Type': 'text/plain; charset=utf-8',
